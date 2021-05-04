90c9916d0e8d843ad77001c8df38186fa38f46b6
Nguyễn Cao Huy Hoàng <61876488+antoinenguyen-09@users.noreply.github.com>
2021-05-04 10:27:16 +0700

    Update sshmonitor.sh

--- sshmonitor.sh
+++ sshmonitor.sh
@@ -2,10 +2,28 @@
 
 cd /var/log || exit 1
 touch checkssh.log || exit 1
-change=`grep -v "$(who)" checkssh.log`
+change=`grep -v "$(who | grep pts)" checkssh.log`
 
 if test change != ""
 then
-	who > checkssh.log
-
+        who | grep pts > checkssh.log
 fi
+
+dir="/var/log/checkssh.log"
+> ssh_mess.log
+rn=1
+while true
+do
+        name=`awk -v var=$rn 'NR==var{printf $1}' ${dir}`
+        hour=`awk -v var=$rn 'NR==var{printf $4}' ${dir}`
+        date=`awk -v var=$rn 'NR==var{printf $3}' ${dir}`
+        message="User ${name} dang nhap thanh cong vao thoi gian ${hour} ${date}"
+        if [[ -z $name ]] || [[ -z $hour ]] || [[ -z $date ]]
+        then
+                exit
+        else
+                echo $message >> ssh_mess.log
+                rn=$((rn+1))
+        fi
+done
+    
